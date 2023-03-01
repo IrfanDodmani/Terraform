21cf5e57499206f4f930c0cebb07468835c9490d
IrfanDodmani <irfandodmani1@gmail.com>
2023-03-02 01:12:01 +0530

    committed

--- scrip.tf
+++ scrip.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "AWSEC2Instance"{
     ami   =  "ami-09ba48996007c8b50"
     instance_type = "t2.micro"
     security_groups = ["Linux"]
-    key_name      = "key1mb"
+    key_name       = "key1mb"
     tags = {
         Name = "EC2 Instance by Terroform"
     }
