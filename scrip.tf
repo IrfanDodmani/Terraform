e05ce8d36a391b0f0916e05475a24bd06da458d2
IrfanDodmani <irfandodmani1@gmail.com>
2023-03-02 01:10:42 +0530

    script updated

---
+++ scrip.tf
@@ -0,0 +1,15 @@
+provider "aws"{
+  region = "ap-south-1"
+  access_key = "AKIAY3PTFVFT4562KAFV"
+  secret_key = "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"  
+}
+
+resource "aws_instance" "AWSEC2Instance"{
+    ami   =  "ami-09ba48996007c8b50"
+    instance_type = "t2.micro"
+    security_groups = ["Linux"]
+    key_name       = "key1mb"
+    tags = {
+        Name = "EC2 Instance by Terroform"
+    }
+}
